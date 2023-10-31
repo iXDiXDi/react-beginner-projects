dd6e5e271b735bbf58e2b330a413f6a0822737ca
Dmitrii Karpenko <ixdixdi@gmail.com>
2023-10-31 19:33:33 +0500

    add quiz logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Error");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+        invites={invites}
+        onClickInvite={onClickInvite}
+      />
       {/* <Success /> */}
     </div>
   );
