861f44c2c69a97bd65ecfcf84e722b4b21f72fce
Dmitrii Karpenko <ixdixdi@gmail.com>
2023-11-02 00:24:42 +0500

    Add another api for fetching data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React, { useEffect, useState } from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.Valute);
+        console.log(json.Valute);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Не удалось получить информацию");
+      });
+  }, []);
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
